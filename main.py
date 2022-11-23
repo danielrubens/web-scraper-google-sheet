e4f2b2d5b0ec72534f51895c95fb4786229c6ff9
Daniel Rubens <51061212+danielrubens@users.noreply.github.com>
2022-11-22 22:58:32 -0300

    Update main.py

--- main.py
+++ main.py
@@ -8,18 +8,12 @@ from writer import DataWriter
 from schedule import repeat, every, run_pending
 
 
-
 if __name__ == "__main__":
 
-                
-    # jurassicPizza
-    # lamafiaPizza
-    store_name = 'jurassicPizza'
+    store_name = 'STORE_NAME'
 
-    #key_gspread = '1Z4Q3QLlxh_VgBfQ_zr8iP0CB5bszSo8tdOrTDHdRr-c'
-    key_gspread = '18jBfYs5sAN1lKiJxYAPeEVIovgc8S_82wwATZ9-KLo4'
-    #credentials_gspread = 'jurassicpizza-api-6147e389352e.json'
-    credentials_gspread = 'my-project-1499973958458-6e8cb64fe192.json'
+    key_gspread = 'GOOGLE_GSPREAD_KEY'
+    credentials_gspread = 'JSON_GSPREAD'
 
     with open(f'{store_name}.json', 'r') as file:
             store = json.load(file)
@@ -29,9 +23,7 @@ if __name__ == "__main__":
     goomer = GoomerStore(drive=driver, credentials=store)
     goomer.login()
 
-
     ws = DataWriter(key_gspread, credentials_gspread)
-
 
     goomer_ingestor = ingestors(
         store=store,
